7dfd71e4d17d7db27d6020a971c950273992e1bc
meikpiep <meik.public@piepmeyer.org>
2021-10-12 20:56:20 +0200

    code clean up and small refactorings

--- app/src/main/java/com/holokenmod/Utils.java
+++ app/src/main/java/com/holokenmod/Utils.java
@@ -1,9 +1,7 @@
 package com.holokenmod;
 
-import android.os.Build;
-
-public class Utils {
-    public static String convertTimetoStr(long time) {
+class Utils {
+    static String convertTimetoStr(long time) {
         int seconds = (int) (time / 1000);
         int minutes = seconds / 60 % 60;
         int hours   = seconds / 3600;
@@ -11,9 +9,4 @@ public class Utils {
 
         return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
-    
-    public static boolean hasHoneycomb() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
-    }
-
 }
