5b0fd0b835c5c50bd866f801b0620af63b351aa9
meikpiep <meik.public@piepmeyer.org>
2021-12-26 11:58:08 +0100

    introduces GridBuilder to simplify creation of grids

--- app/src/androidTest/java/TestCageBacktrack.java
+++ app/src/androidTest/java/TestCageBacktrack.java
@@ -5,6 +5,7 @@ import com.holokenmod.Grid;
 import com.holokenmod.GridCage;
 import com.holokenmod.GridCageAction;
 import com.holokenmod.backtrack.hybrid.MathDokuCage2BackTrack;
+import com.holokenmod.creation.GridBuilder;
 import com.holokenmod.options.DigitSetting;
 import com.holokenmod.options.GameVariant;
 
@@ -20,44 +21,14 @@ public class TestCageBacktrack {
 		GameVariant.getInstance().setDigitSetting(DigitSetting.FIRST_DIGIT_ONE);
 		GameVariant.getInstance().setShowOperators(true);
 		
-		Grid grid = new Grid(3);
+		GridBuilder builder = new GridBuilder(3, 3);
 		
-		grid.addAllCells();
-		
-		GridCage cage = new GridCage(grid);
-		cage.setCageId(0);
-		cage.setAction(GridCageAction.ACTION_SUBTRACT);
-		cage.setResult(1);
-		cage.addCell(grid.getCell(0));
-		cage.addCell(grid.getCell(3));
-		grid.addCage(cage);
+		builder.addCage(1, GridCageAction.ACTION_SUBTRACT, 0, 3)
+			.addCage(3, GridCageAction.ACTION_MULTIPLY, 1, 2)
+			.addCage(4, GridCageAction.ACTION_MULTIPLY, 4, 5, 8)
+			.addCage(3, GridCageAction.ACTION_DIVIDE, 6, 7);
 
-		cage = new GridCage(grid);
-		cage.setCageId(1);
-		cage.setAction(GridCageAction.ACTION_MULTIPLY);
-		cage.setResult(3);
-		cage.addCell(grid.getCell(1));
-		cage.addCell(grid.getCell(2));
-		grid.addCage(cage);
-		
-		cage = new GridCage(grid);
-		cage.setCageId(2);
-		cage.setAction(GridCageAction.ACTION_MULTIPLY);
-		cage.setResult(4);
-		cage.addCell(grid.getCell(4));
-		cage.addCell(grid.getCell(5));
-		cage.addCell(grid.getCell(8));
-		grid.addCage(cage);
-		
-		cage = new GridCage(grid);
-		cage.setCageId(3);
-		cage.setAction(GridCageAction.ACTION_DIVIDE);
-		cage.setResult(3);
-		cage.addCell(grid.getCell(6));
-		cage.addCell(grid.getCell(7));
-		grid.addCage(cage);
-		
-		grid.setCageTexts();
+		Grid grid = builder.createGrid();
 		
 		System.out.println(grid.toString());
 		
