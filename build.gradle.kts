5cc573094e41bd87fb01cbc41cafd83cfa568409
meikpiep <meik.public@piepmeyer.org>
2023-05-21 21:56:10 +0200

    Updates Kotlin version from 1.8.0 to 1.8.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,8 @@ buildscript {
 plugins {
     id("com.android.application").version("8.0.1") apply false
     id("com.android.library").version("8.0.1") apply false
-    id("org.jetbrains.kotlin.android").version("1.8.0") apply false
+    id("org.jetbrains.kotlin.android").version("1.8.20") apply false
+    id("org.jetbrains.kotlin.jvm").version("1.8.20") apply false
     id("org.sonarqube").version("4.0.0.2929")
     id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
 }
