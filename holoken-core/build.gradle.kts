09cae4d03fd309a828c7e097220590380c76ca80
meikpiep <meik.public@piepmeyer.org>
2023-10-03 21:42:37 +0200

    Upgrades Kotest from 5.6.2 to 5.7.2

--- holoken-core/build.gradle.kts
+++ holoken-core/build.gradle.kts
@@ -29,9 +29,10 @@ dependencies {
     testApi("io.github.oshai:kotlin-logging-jvm:5.0.2")
     api("org.slf4j:slf4j-simple:2.0.7")
 
-    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
-    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
-    testImplementation("io.kotest:kotest-framework-datatest:5.6.2")
+    testImplementation(platform("io.kotest:kotest-bom:5.7.2"))
+    testImplementation("io.kotest:kotest-runner-junit5")
+    testImplementation("io.kotest:kotest-assertions-core")
+    testImplementation("io.kotest:kotest-framework-datatest")
 }
 
 java {
@@ -62,9 +63,10 @@ testing {
                 implementation("io.github.oshai:kotlin-logging-jvm:5.0.2")
                 implementation("org.slf4j:slf4j-simple:2.0.7")
 
-                implementation("io.kotest:kotest-runner-junit5:5.6.2")
-                implementation("io.kotest:kotest-assertions-core:5.6.2")
-                implementation("io.kotest:kotest-framework-datatest:5.6.2")
+                implementation(platform("io.kotest:kotest-bom:5.7.2"))
+                implementation("io.kotest:kotest-runner-junit5")
+                implementation("io.kotest:kotest-assertions-core")
+                implementation("io.kotest:kotest-framework-datatest")
             }
 
             targets {
