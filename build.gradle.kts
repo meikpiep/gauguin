040f2366537e5b88460b369e42046add8e3b185d
meikpiep <meik.public@piepmeyer.org>
2023-10-03 22:24:23 +0200

    Updates SonarQube from 4.3.x to 4.4.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("com.android.library").version("8.1.1") apply false
     id("org.jetbrains.kotlin.android").version("1.9.10") apply false
     id("org.jetbrains.kotlin.jvm").version("1.9.10") apply false
-    id("org.sonarqube").version("4.3.1.3277")
+    id("org.sonarqube").version("4.4.1.3373")
     id("org.jlleitschuh.gradle.ktlint") version "11.6.0"
 }
 
