2288b57b0f7d0f3fcf5aa6efbc79e82236128651
meikpiep <meik.public@piepmeyer.org>
2023-11-08 20:06:04 +0100

    Update Kotest from 5.7.2 to 5.8.0

--- gauguin-core/build.gradle.kts
+++ gauguin-core/build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     testApi("io.github.oshai:kotlin-logging-jvm:5.1.0")
     api("org.slf4j:slf4j-simple:2.0.9")
 
-    testImplementation(platform("io.kotest:kotest-bom:5.7.2"))
+    testImplementation(platform("io.kotest:kotest-bom:5.8.0"))
     testImplementation("io.kotest:kotest-runner-junit5")
     testImplementation("io.kotest:kotest-assertions-core")
     testImplementation("io.kotest:kotest-framework-datatest")
@@ -62,7 +62,7 @@ testing {
                 implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
                 implementation("org.slf4j:slf4j-simple:2.0.9")
 
-                implementation(platform("io.kotest:kotest-bom:5.7.2"))
+                implementation(platform("io.kotest:kotest-bom:5.8.0"))
                 implementation("io.kotest:kotest-runner-junit5")
                 implementation("io.kotest:kotest-assertions-core")
                 implementation("io.kotest:kotest-framework-datatest")
