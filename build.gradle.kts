a3ddd0086202aea6bf7224e81f6c7bbd54968d14
meikpiep <meik.public@piepmeyer.org>
2023-11-12 15:20:52 +0100

    Update Android Gradle Plugin from 8.1.2 to 8.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("com.android.tools.build:gradle:8.1.3")
     }
 }
 plugins {
-    id("com.android.application").version("8.1.2") apply false
-    id("com.android.library").version("8.1.2") apply false
+    id("com.android.application").version("8.1.3") apply false
+    id("com.android.library").version("8.1.3") apply false
     id("org.jetbrains.kotlin.android").version("1.9.10") apply false
     id("org.jetbrains.kotlin.jvm").version("1.9.10") apply false
     id("org.sonarqube").version("4.4.1.3373")
