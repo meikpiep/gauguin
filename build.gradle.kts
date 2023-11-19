13cc2e55f101c42c67f965ab0bb684c8bfe36a00
meikpiep <meik.public@piepmeyer.org>
2023-11-19 21:14:08 +0100

    Correct report path of Android linter to SonarQube

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ sonarqube {
         property("sonar.projectKey", "org.piepmeyer.gauguin")
         property("sonar.organization", "meikpiep")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.androidLint.reportPaths", "gauguin-app/build/reports")
+        property("sonar.androidLint.reportPaths", "gauguin-app/build/reports/lint-results-debug.xml")
     }
 }
 
