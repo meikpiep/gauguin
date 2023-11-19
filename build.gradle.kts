adc736c8aaf0c8188240ab05bd995f88456a6517
meikpiep <meik.public@piepmeyer.org>
2023-11-19 21:06:04 +0100

    Update Kotlin version from 1.9.10 to 1.9.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
 plugins {
     id("com.android.application").version("8.1.4") apply false
     id("com.android.library").version("8.1.4") apply false
-    id("org.jetbrains.kotlin.android").version("1.9.10") apply false
-    id("org.jetbrains.kotlin.jvm").version("1.9.10") apply false
+    id("org.jetbrains.kotlin.android").version("1.9.20") apply false
+    id("org.jetbrains.kotlin.jvm").version("1.9.20") apply false
     id("org.sonarqube").version("4.4.1.3373")
     id("org.jlleitschuh.gradle.ktlint") version "11.6.0"
 }
