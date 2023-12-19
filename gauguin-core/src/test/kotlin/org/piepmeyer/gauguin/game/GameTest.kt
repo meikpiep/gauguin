32ad2c351aa7668570b088e1aea57cf08c707ab7
meikpiep <meik.public@piepmeyer.org>
2023-12-19 20:22:03 +0100

    Fix failing test by switching it to test Game class instead of GameSolveService

--- gauguin-core/src/test/kotlin/org/piepmeyer/gauguin/game/GameTest.kt
+++ gauguin-core/src/test/kotlin/org/piepmeyer/gauguin/game/GameTest.kt
@@ -4,9 +4,12 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.shouldBe
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import io.mockk.mockkClass
+import io.mockk.runs
 import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 import org.koin.test.KoinTest
 import org.koin.test.mock.MockProvider
 import org.koin.test.mock.declareMock
@@ -15,6 +18,7 @@ import org.piepmeyer.gauguin.creation.cage.GridCageType
 import org.piepmeyer.gauguin.grid.GridCageAction
 import org.piepmeyer.gauguin.grid.GridCell
 import org.piepmeyer.gauguin.preferences.ApplicationPreferences
+import org.piepmeyer.gauguin.preferences.StatisticsManager
 
 class GameTest : FunSpec(), KoinTest {
     init {
@@ -48,6 +52,49 @@ class GameTest : FunSpec(), KoinTest {
 
             smallGrid.cells.forEach { it.userValue shouldBe GridCell.NO_VALUE_SET }
             smallGrid.cells.forEach { it.possibles.shouldBeEmpty() }
+
+            stopKoin()
+        }
+
+        test("revealing a cell sets the correct user value and clears possible numbers") {
+
+            startKoin {}
+
+            declareMock<ApplicationPreferences> {
+                every { showDupedDigits() } returns true
+                every { removePencils() } returns true
+            }
+            declareMock<StatisticsManager> {
+                every { puzzleStartedToBePlayed() } just runs
+            }
+
+            val smallGrid =
+                GridBuilder(2)
+                    .addCage(2, GridCageAction.ACTION_MULTIPLY, GridCageType.ANGLE_RIGHT_BOTTOM, 0)
+                    .addSingleCage(2, 3)
+                    .createGrid()
+            smallGrid.isActive = true
+
+            val cellToReveal = smallGrid.cells[0]
+
+            cellToReveal.addPossible(1)
+            cellToReveal.addPossible(2)
+            cellToReveal.value = 2
+            cellToReveal.userValue = 1
+            smallGrid.selectedCell = cellToReveal
+
+            val game = Game(
+                grid = smallGrid,
+                mockk(relaxed = true),
+                mockk(relaxed = true)
+            )
+
+            game.revealCell(cellToReveal)
+
+            cellToReveal.possibles.shouldBeEmpty()
+            cellToReveal.userValue shouldBe 2
+
+            stopKoin()
         }
     }
 }
