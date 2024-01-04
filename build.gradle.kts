bbe51e3c54c7f9e93b2445bdf2c57a61847e918c
meikpiep <meik.public@piepmeyer.org>
2024-01-04 17:33:55 +0100

    Upgrade Android Grade Plugin from 8.2.0 to 8.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("com.android.tools.build:gradle:8.2.1")
     }
 }
 plugins {
-    id("com.android.application").version("8.2.0") apply false
-    id("com.android.library").version("8.2.0") apply false
+    id("com.android.application").version("8.2.1") apply false
+    id("com.android.library").version("8.2.1") apply false
     id("org.jetbrains.kotlin.android").version("1.9.21") apply false
     id("org.jetbrains.kotlin.jvm").version("1.9.21") apply false
     id("org.sonarqube").version("4.4.1.3373")
