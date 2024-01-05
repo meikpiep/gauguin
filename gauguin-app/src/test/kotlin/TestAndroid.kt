cc012a9427abd04614577c536c9b05e6c1005bd1
meikpiep <meik.public@piepmeyer.org>
2024-01-05 10:15:25 +0100

    wip

---
+++ gauguin-app/src/test/kotlin/TestAndroid.kt
@@ -0,0 +1,12 @@
+
+import io.kotest.core.spec.style.FunSpec
+import io.mockk.mockk
+import org.piepmeyer.gauguin.ui.grid.GridLayoutDetails
+
+class TestAndroid :
+    FunSpec({
+
+        test("sample 2") {
+            GridLayoutDetails(Pair(1.0f, 1.0f), mockk(relaxed = true))
+        }
+    })
