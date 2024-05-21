1882fb4de70ff0bdd042908aa254f44e242e7c0f
meikpiep <meik.public@piepmeyer.org>
2024-05-21 18:31:56 +0200

    Add test of new algorithm

--- gauguin-core/src/test/kotlin/org/piepmeyer/gauguin/creation/MergingCageGridCalculatorTest.kt
+++ gauguin-core/src/test/kotlin/org/piepmeyer/gauguin/creation/MergingCageGridCalculatorTest.kt
@@ -6,6 +6,7 @@ import org.piepmeyer.gauguin.difficulty.GridDifficultyCalculator
 import org.piepmeyer.gauguin.grid.GridSize
 import org.piepmeyer.gauguin.options.GameOptionsVariant
 import org.piepmeyer.gauguin.options.GameVariant
+import org.piepmeyer.gauguin.options.GridCageOperation
 
 private val logger = KotlinLogging.logger {}
 
@@ -18,6 +19,28 @@ class MergingCageGridCalculatorTest : FunSpec({
         val calculator =
             MergingCageGridCalculator(
                 variant = GameVariant(GridSize(6, 6), GameOptionsVariant.createClassic()),
+                randomizer = randomizer,
+                shuffler = RandomPossibleDigitsShuffler(randomizer.random),
+            )
+
+        val grid = calculator.calculate()
+
+        logger.info { grid }
+
+        logger.info { GridDifficultyCalculator(grid).calculate() }
+    }
+
+    xtest("9x9 with plus and minus gets calculated") {
+
+        val randomizer = SeedRandomizerMock(0)
+
+        val calculator =
+            MergingCageGridCalculator(
+                variant =
+                    GameVariant(
+                        GridSize(9, 9),
+                        GameOptionsVariant.createClassic().copy(cageOperation = GridCageOperation.OPERATIONS_ADD_SUB),
+                    ),
                 randomizer = randomizer,
                 shuffler = RandomPossibleDigitsShuffler(randomizer.random),
             )
