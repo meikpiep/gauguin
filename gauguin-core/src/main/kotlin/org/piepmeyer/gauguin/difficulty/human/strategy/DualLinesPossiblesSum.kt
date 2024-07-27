5093430d8a89a4c9fd101ca8df9dcff8a521945b
meikpiep <meik.public@piepmeyer.org>
2024-07-27 13:57:02 +0200

    wip dual lines

---
+++ gauguin-core/src/main/kotlin/org/piepmeyer/gauguin/difficulty/human/strategy/DualLinesPossiblesSum.kt
@@ -0,0 +1,47 @@
+package org.piepmeyer.gauguin.difficulty.human.strategy
+
+import org.piepmeyer.gauguin.difficulty.human.HumanSolverStrategy
+import org.piepmeyer.gauguin.grid.Grid
+
+class DualLinesPossiblesSum : HumanSolverStrategy {
+    override fun fillCells(grid: Grid): Boolean {
+        /*val linePairs = GridLines(grid).adjactPairsOflinesWithEachPossibleValue()
+
+        linePairs.forEach { linePair ->
+            linePair
+                .cages()
+                .filter { it.cells.any { !it.isUserValueSet } }
+                .forEach { cage ->
+
+                    val validPossibles =
+                        ValidPossiblesCalculator(grid, cage)
+                            .calculatePossibles()
+                            .map {
+                                it.filterIndexed { index, _ ->
+                                    line.contains(cage.cells[index])
+                                }
+                            }
+
+                    if (validPossibles.isNotEmpty()) {
+                        val possibleDigitsAlwaysInLine =
+                            grid.variant.possibleDigits.filter { possible ->
+                                validPossibles.all { it.contains(possible) }
+                            }
+
+                        if (deletePossibleInSingleCage(
+                                line,
+                                cage,
+                                possibleDigitsAlwaysInLine,
+                            )
+                        ) {
+                            return true
+                        }
+                    }
+                }
+        }*/
+
+        return false
+    }
+
+    override fun difficulty(): Int = 38
+}
