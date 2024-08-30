a62cdbcd4a173c0ed95513240ec736bac8938de8
meikpiep <meik.public@piepmeyer.org>
2024-08-30 14:49:52 +0200

    Upgrade Android Gradle plugin from 8.5.2 to 8.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.net.URI
 
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.5.2")
+        classpath("com.android.tools.build:gradle:8.6.0")
     }
 }
 
