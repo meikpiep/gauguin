7dd603b29a4deb17caf9b0c3d1c6bba84b9ac2dd
meikpiep <meik.public@piepmeyer.org>
2024-09-24 19:57:29 +0200

    Upgrade Kotlin from 1.9.23 to 2.0.20
    Includes upgrades of Kotlin coroutines, serialization, kotlin-logging, ksp.

--- gauguin-core/build.gradle.kts
+++ gauguin-core/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("org.jetbrains.kotlin.jvm")
     jacoco
     `jvm-test-suite`
-    kotlin("plugin.serialization") version "1.9.21"
+    kotlin("plugin.serialization") version "2.0.20"
     id("com.google.devtools.ksp")
 }
 
