eb1e279f4e3b2008011683896715ee09edf2d077
meikpiep <meik.public@piepmeyer.org>
2024-10-03 20:23:08 +0200

    bug fix

--- gauguin-core/src/main/kotlin/org/piepmeyer/gauguin/difficulty/human/strategy/SinglePossibleInLine.kt
+++ gauguin-core/src/main/kotlin/org/piepmeyer/gauguin/difficulty/human/strategy/SinglePossibleInLine.kt
@@ -1,30 +1,30 @@
 package org.piepmeyer.gauguin.difficulty.human.strategy
 
+import org.piepmeyer.gauguin.difficulty.human.GridLines
 import org.piepmeyer.gauguin.difficulty.human.HumanSolverStrategy
 import org.piepmeyer.gauguin.grid.Grid
 
 class SinglePossibleInLine : HumanSolverStrategy {
     override fun fillCells(grid: Grid): Boolean {
-        grid.cells
-            .filter { !it.isUserValueSet }
-            .forEach { cell ->
+        GridLines(grid).linesWithEachPossibleValue().forEach { line ->
+            line
+                .cells()
+                .filter { !it.isUserValueSet }
+                .forEach { cell ->
+                    val otherCellsInLine = line.cells() - cell
 
-                cell.possibles.forEach { possible ->
-                    if (grid
-                            .getCellsAtSameRow(cell)
-                            .map {
-                                it.possibles
-                            }.none {
-                                it.contains(possible)
-                            } ||
-                        grid.getCellsAtSameColumn(cell).map { it.possibles }.none { it.contains(possible) }
-                    ) {
-                        grid.setUserValueAndRemovePossibles(cell, possible)
+                    cell.possibles.forEach { possible ->
+                        if (otherCellsInLine
+                                .map { it.possibles }
+                                .none { it.contains(possible) }
+                        ) {
+                            grid.setUserValueAndRemovePossibles(cell, possible)
 
-                        return true
+                            return true
+                        }
                     }
                 }
-            }
+        }
 
         return false
     }
