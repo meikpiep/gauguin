89de8ac22b0ffbfa4dfbcd99150935801d764f51
meikpiep <meik.public@piepmeyer.org>
2024-11-03 18:38:44 +0100

    wip

--- gauguin-core/src/main/kotlin/org/piepmeyer/gauguin/difficulty/human/strategy/NakedPair.kt
+++ gauguin-core/src/main/kotlin/org/piepmeyer/gauguin/difficulty/human/strategy/NakedPair.kt
@@ -1,17 +1,14 @@
 package org.piepmeyer.gauguin.difficulty.human.strategy
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import org.piepmeyer.gauguin.difficulty.human.HumanSolverStrategy
 import org.piepmeyer.gauguin.difficulty.human.PossiblesCache
 import org.piepmeyer.gauguin.grid.Grid
 import org.piepmeyer.gauguin.grid.GridCell
 
-private val logger = KotlinLogging.logger {}
-
 /**
- * Finds a naked pair, that is two cell in the same row or column which have to same list of
+ * Finds a naked pair, that is two cells in the same row or column which have to same list of
  * exactly two possible values. As these values could not occur in any other cells beside these
- * two, these values get deletes from the other cages possibles.
+ * two, these values get deleted from the other cages possibles.
  */
 class NakedPair : HumanSolverStrategy {
     override fun fillCells(
@@ -41,8 +38,6 @@ class NakedPair : HumanSolverStrategy {
                         cellsWithPossibles.forEach {
                             it.possibles -= possibles
                         }
-
-                        logger.trace { "Naked pair found: ${cell.cellNumber} and ${otherCell.cellNumber}" }
 
                         return true
                     }
