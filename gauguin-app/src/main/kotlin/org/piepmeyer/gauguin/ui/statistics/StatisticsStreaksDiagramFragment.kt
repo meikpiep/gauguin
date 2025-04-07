9aa5c9df11f1cc9b39a199ab76df545cd07ca854
meikpiep <meik.public@piepmeyer.org>
2025-04-07 21:57:46 +0200

    Mark current game at streaks diagram

--- gauguin-app/src/main/kotlin/org/piepmeyer/gauguin/ui/statistics/StatisticsStreaksDiagramFragment.kt
+++ gauguin-app/src/main/kotlin/org/piepmeyer/gauguin/ui/statistics/StatisticsStreaksDiagramFragment.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.color.MaterialColors
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianChartModel
 import com.patrykandpatrick.vico.core.cartesian.data.ColumnCartesianLayerModel
+import com.patrykandpatrick.vico.core.cartesian.layer.ColumnCartesianLayer
+import com.patrykandpatrick.vico.core.common.component.LineComponent
+import com.patrykandpatrick.vico.core.common.data.ExtraStore
+import com.patrykandpatrick.vico.views.cartesian.CartesianChartView
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.piepmeyer.gauguin.R
@@ -28,19 +33,29 @@ class StatisticsStreaksDiagramFragment :
         binding = FragmentStatisticsStreaksDiagramBinding.inflate(inflater, parent, false)
 
         val streakSequence =
-            statisticsManager.statistics().overall.streakSequence.map {
-                if (it == 0) {
-                    0.1
-                } else {
-                    it
-                }
+            statisticsManager
+                .statistics()
+                .overall.streakSequence
+                .map {
+                    if (it == 0) {
+                        0.1.toFloat()
+                    } else {
+                        it.toFloat()
+                    }
+                }.toMutableList()
+
+        val filteredStreaks =
+            streakSequence.filterIndexed { index, number ->
+                (index == streakSequence.size - 1 || index == 0) ||
+                    number == 0f ||
+                    (streakSequence[index - 1] >= number || streakSequence[index + 1] <= number)
             }
 
         val filledUpStreakSequence =
-            if (streakSequence.size >= 8) {
-                streakSequence
+            if (filteredStreaks.size >= 8) {
+                filteredStreaks
             } else {
-                streakSequence + List(8 - streakSequence.size) { 0F }
+                filteredStreaks + List(8 - filteredStreaks.size) { 0F }
             }
 
         binding.overallStreaks.setModel(
@@ -51,6 +66,50 @@ class StatisticsStreaksDiagramFragment :
             ),
         )
 
+        addColorToLine(binding.overallStreaks, filteredStreaks.size - 1)
+
         return binding.root
+    }
+
+    private fun addColorToLine(
+        chartView: CartesianChartView,
+        indexCurrentStreak: Int,
+    ) {
+        val formerStreaksColumn =
+            LineComponent(
+                color = MaterialColors.getColor(binding.overallStreaks, com.google.android.material.R.attr.colorSecondary),
+                thicknessDp = 8f,
+            )
+        val currentStreakColumn =
+            LineComponent(
+                color = MaterialColors.getColor(binding.overallStreaks, R.attr.colorCustomColor1),
+                thicknessDp = 8f,
+            )
+
+        with(chartView) {
+            chart =
+                chart!!.copy(
+                    (chart!!.layers[0] as ColumnCartesianLayer).copy(
+                        columnProvider = getColumnProvider(indexCurrentStreak, formerStreaksColumn, currentStreakColumn),
+                    ),
+                )
+        }
+    }
+
+    private fun getColumnProvider(
+        indexCurrentStreak: Int,
+        formerStreaksColumn: LineComponent,
+        currentStreakColumn: LineComponent,
+    ) = object : ColumnCartesianLayer.ColumnProvider {
+        override fun getColumn(
+            entry: ColumnCartesianLayerModel.Entry,
+            seriesIndex: Int,
+            extraStore: ExtraStore,
+        ) = if (entry.x.toInt() == indexCurrentStreak) currentStreakColumn else formerStreaksColumn
+
+        override fun getWidestSeriesColumn(
+            seriesIndex: Int,
+            extraStore: ExtraStore,
+        ) = formerStreaksColumn
     }
 }
