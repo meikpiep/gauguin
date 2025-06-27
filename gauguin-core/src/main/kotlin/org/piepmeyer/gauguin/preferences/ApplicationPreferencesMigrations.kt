784f9e9781d91e334940d2faf0c1bcdd412d3e5f
meikpiep <meik.public@piepmeyer.org>
2025-06-27 18:29:50 +0200

    Refactor: Move enums NightMode and Theme to package 'preferences'

--- gauguin-core/src/main/kotlin/org/piepmeyer/gauguin/preferences/ApplicationPreferencesMigrations.kt
+++ gauguin-core/src/main/kotlin/org/piepmeyer/gauguin/preferences/ApplicationPreferencesMigrations.kt
@@ -1,7 +1,5 @@
 package org.piepmeyer.gauguin.preferences
 
-import org.piepmeyer.gauguin.NightMode
-import org.piepmeyer.gauguin.Theme
 import org.piepmeyer.gauguin.options.DifficultySetting
 
 class ApplicationPreferencesMigrations(
