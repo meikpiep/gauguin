0fb3688310859645c3ba1ccc904e55739a27f693
meikpiep <meik.public@piepmeyer.org>
2025-07-14 18:55:22 +0200

    Upgrade Android Gradle Plugin from version 8.11.0 to 8.11.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.net.URI
 
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.11.0")
+        classpath("com.android.tools.build:gradle:8.11.1")
     }
 }
 
