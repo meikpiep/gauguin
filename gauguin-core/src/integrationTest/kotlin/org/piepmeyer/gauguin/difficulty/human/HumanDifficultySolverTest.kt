ce8ff4a85a72e0e76e811f56f0e0f47fe6a0193e
meikpiep <meik.public@piepmeyer.org>
2025-08-19 17:12:41 +0200

    human solver: update solve rates via tests

--- gauguin-core/src/integrationTest/kotlin/org/piepmeyer/gauguin/difficulty/human/HumanDifficultySolverTest.kt
+++ gauguin-core/src/integrationTest/kotlin/org/piepmeyer/gauguin/difficulty/human/HumanDifficultySolverTest.kt
@@ -7,21 +7,19 @@ import io.kotest.matchers.shouldBe
 import org.piepmeyer.gauguin.creation.MergingCageGridCalculator
 import org.piepmeyer.gauguin.creation.RandomPossibleDigitsShuffler
 import org.piepmeyer.gauguin.creation.SeedRandomizerMock
-import org.piepmeyer.gauguin.game.save.SaveGame
 import org.piepmeyer.gauguin.grid.GridSize
 import org.piepmeyer.gauguin.options.GameOptionsVariant
 import org.piepmeyer.gauguin.options.GameVariant
-import java.io.File
 
 class HumanDifficultySolverTest :
     FunSpec({
-        for (seed in 0..99) {
+        for (seed in 0..999) {
             // 10_000 of 4x4, random: 6 left unsolved
             // 10_000 of 4x4, merge: 19 left unsolved
             // 10_000 of 5x5, merge: 37 left unsolved
             // 10_000 of 2x4, merge: no (!) left unsolved
-            // 10_000 of 3x4, merge: 40 left unsolved
-            //  1_000 of 3x6, merge: 79 left unsolved
+            // 10_000 of 3x4, merge: 37 left unsolved
+            //  1_000 of 3x6, merge: 73 left unsolved
             //  1_000 of 6x6, merge: 27 left unsolved
             //    100 of 9x9, merge: 17 left unsolved
             //     10 of 11x11, merge: left unsolved
@@ -32,7 +30,7 @@ class HumanDifficultySolverTest :
                     val calculator =
                         MergingCageGridCalculator(
                             GameVariant(
-                                GridSize(9, 9),
+                                GridSize(3, 6),
                                 GameOptionsVariant.createClassic(),
                             ),
                             randomizer,
@@ -52,7 +50,7 @@ class HumanDifficultySolverTest :
                         if (grid.numberOfMistakes() != 0) {
                             throw IllegalStateException("Found a grid with wrong values.")
                         }
-                        grid.isActive = true
+                        /*grid.isActive = true
                         grid.startedToBePlayed = true
                         grid.description = "${grid.gridSize.width}x${grid.gridSize.height}-$seed"
                         val saveGame =
@@ -63,7 +61,7 @@ class HumanDifficultySolverTest :
                                 ),
                             )
 
-                        saveGame.save(grid)
+                        saveGame.save(grid)*/
                     }
 
                     solverResult.success shouldBe true
