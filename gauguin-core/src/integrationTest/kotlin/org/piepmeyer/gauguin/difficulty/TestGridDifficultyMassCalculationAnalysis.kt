a17ff49214893f039908dd5c5e4adba8c8dd2147
meikpiep <meik.public@piepmeyer.org>
2025-09-08 21:08:50 +0200

    upgrade Kotest from 6.0.1 to 6.0.3

--- gauguin-core/src/integrationTest/kotlin/org/piepmeyer/gauguin/difficulty/TestGridDifficultyMassCalculationAnalysis.kt
+++ gauguin-core/src/integrationTest/kotlin/org/piepmeyer/gauguin/difficulty/TestGridDifficultyMassCalculationAnalysis.kt
@@ -1,7 +1,6 @@
 package org.piepmeyer.gauguin.difficulty
 
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.resource.resourceAsString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.piepmeyer.gauguin.creation.GameVariantMassDifficultyItem
@@ -11,7 +10,8 @@ import kotlin.math.roundToInt
 class TestGridDifficultyMassCalculationAnalysis :
     FunSpec({
         xtest("calculateValues") {
-            val calculatedDifficulties = resourceAsString("/org/piepmeyer/gauguin/difficulty/mass-difficulties.yml")
+            val pathDifficultyFile = "/org/piepmeyer/gauguin/difficulty/mass-difficulties.yml"
+            val calculatedDifficulties = this::class.java.getResource(pathDifficultyFile)!!.readText()
 
             val entries = Json.decodeFromString<List<GameVariantMassDifficultyItem>>(calculatedDifficulties)
 
