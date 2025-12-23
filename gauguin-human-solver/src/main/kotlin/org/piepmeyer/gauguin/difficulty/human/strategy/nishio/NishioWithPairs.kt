39fa026449ea0fae3b0af9973b3072591727d416
meikpiep <meik.public@piepmeyer.org>
2025-12-23 13:33:02 +0100

    implement human solver strategy "nishio with pairs"

---
+++ gauguin-human-solver/src/main/kotlin/org/piepmeyer/gauguin/difficulty/human/strategy/nishio/NishioWithPairs.kt
@@ -0,0 +1,153 @@
+package org.piepmeyer.gauguin.difficulty.human.strategy.nishio
+
+import org.piepmeyer.gauguin.creation.cage.GridSingleCageCreator
+import org.piepmeyer.gauguin.difficulty.human.HumanSolverCache
+import org.piepmeyer.gauguin.difficulty.human.HumanSolverStrategy
+import org.piepmeyer.gauguin.difficulty.human.HumanSolverStrategyResult
+import org.piepmeyer.gauguin.grid.Grid
+import org.piepmeyer.gauguin.grid.GridCage
+import org.piepmeyer.gauguin.grid.GridCell
+
+sealed interface NishioResult {
+    class NothingFound : NishioResult
+
+    class Contradictions : NishioResult
+
+    class Solved(
+        val solvedGrid: Grid,
+    ) : NishioResult
+}
+
+/**
+ * Uses nishio (guessing) with cells containing two or less possible numbers.
+ *
+ * For all cells with two possible numbers, each number is used to utilize nishio with the given
+ * number. If the algorithm finds
+ *   - a contradiction (meaning the conclusions from this nishio try lead to an impossible state)
+ *     --> the other possbiel number gets put into the cell
+ *   - all remaining cells can be filled without a contradiction
+ *     --> we found the solution of the entire grid, put in the values, the grid was solved
+ *
+ * One possible nishio cell with one possible value is executed as follow:
+ *   - put in the possible number into the nishio cell, removing all possible values of other
+ *     affected cells
+ *   - while there is a cell with exactly one possible value, put in this value, removing all
+ *     possible values of other affected cells
+ *     - check if the math of the affected cage is invalid, if this is the case: terminate with no
+ *       finding
+ *   - if there is no cell with a single possible:
+ *     - remove all possible values of each cage if the possible is no longer valid
+ */
+class NishioWithPairs : HumanSolverStrategy {
+    override fun fillCells(
+        grid: Grid,
+        cache: HumanSolverCache,
+    ): HumanSolverStrategyResult {
+        grid.cells
+            .filter { it.possibles.size == 2 }
+            .forEach { cell ->
+                cell.possibles.forEach { possible ->
+                    val result = tryWithNishio(grid, cell, possible)
+
+                    if (result is NishioResult.Contradictions) {
+                        val changedCells = grid.setUserValueAndRemovePossibles(cell, cell.possibles.first { it != possible })
+
+                        return HumanSolverStrategyResult.Success(changedCells)
+                    } else if (result is NishioResult.Solved) {
+                        val cellsWithoutUserValue = grid.cells.filter { !it.isUserValueSet }
+                        cellsWithoutUserValue.forEach { cell ->
+                            cell.userValue = result.solvedGrid.getCell(cell.cellNumber).userValue
+                        }
+
+                        return HumanSolverStrategyResult.Success(cellsWithoutUserValue)
+                    }
+                }
+            }
+
+        return HumanSolverStrategyResult.NothingChanged()
+    }
+
+    fun tryWithNishio(
+        grid: Grid,
+        cell: GridCell,
+        possible: Int,
+    ): NishioResult {
+        val tryGrid = grid.copyWithEmptyUserValues()
+
+        grid.cells.forEach {
+            val tryCell = tryGrid.getCell(it.cellNumber)
+
+            if (it.userValue != null) {
+                tryCell.userValue = it.userValue
+            } else {
+                tryCell.possibles = it.possibles
+            }
+        }
+
+        tryGrid.setUserValueAndRemovePossibles(tryGrid.getCell(cell.cellNumber), possible)
+
+        println(tryGrid)
+
+        do {
+            val cellWithSinglePossible = tryGrid.cells.firstOrNull { it.possibles.size == 1 }
+
+            if (cellWithSinglePossible == null) {
+                val cageWithEmptyCells = tryGrid.cages.filter { it.cells.any { cell -> !cell.isUserValueSet } }
+
+                if (tryGrid.cells.all { it.isUserValueSet }) {
+                    return NishioResult.Solved(tryGrid)
+                }
+
+                val deletedPossibles = tryToDeletePossibles(cageWithEmptyCells, grid)
+
+                if (!deletedPossibles) {
+                    return NishioResult.NothingFound()
+                }
+            } else {
+                tryGrid.setUserValueAndRemovePossibles(
+                    cell = cellWithSinglePossible,
+                    value = cellWithSinglePossible.possibles.first(),
+                )
+
+                if (!cellWithSinglePossible.cage().isUserMathCorrect()) {
+                    return NishioResult.Contradictions()
+                }
+            }
+        } while (true)
+    }
+
+    private fun tryToDeletePossibles(
+        cageWithEmptyCells: List<GridCage>,
+        grid: Grid,
+    ): Boolean {
+        var deletedPossibles = false
+
+        cageWithEmptyCells.forEach { cage ->
+            val creator = GridSingleCageCreator(grid.variant, cage)
+
+            val combinations = creator.possibleCombinations
+
+            val newPossibles =
+                combinations
+                    .filter {
+                        cage.cells.withIndex().all { cell ->
+                            if (cell.value.isUserValueSet) {
+                                cell.value.userValue == it[cell.index]
+                            } else {
+                                cell.value.possibles.contains(it[cell.index])
+                            }
+                        }
+                    }.toSet()
+
+            cage.cells.forEachIndexed { cellIndex, cell ->
+                cell.possibles.forEach { possible ->
+                    if (newPossibles.none { it[cellIndex] == possible }) {
+                        cell.removePossible(possible)
+                        deletedPossibles = true
+                    }
+                }
+            }
+        }
+        return deletedPossibles
+    }
+}
---
+++ gauguin-human-solver/src/test/kotlin/org/piepmeyer/gauguin/difficulty/human/strategy/nishio/NishioWithPairsTest.kt
@@ -0,0 +1,163 @@
+package org.piepmeyer.gauguin.difficulty.human.strategy.nishio
+
+import io.kotest.assertions.withClue
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.shouldBe
+import org.piepmeyer.gauguin.creation.GridBuilder
+import org.piepmeyer.gauguin.creation.cage.GridCageType
+import org.piepmeyer.gauguin.difficulty.human.HumanSolverStrategyResult
+import org.piepmeyer.gauguin.grid.Grid
+import org.piepmeyer.gauguin.options.DigitSetting
+import org.piepmeyer.gauguin.options.GameOptionsVariant
+
+class NishioWithPairsTest :
+    FunSpec({
+
+        test("no nishio gets found") {
+            val grid =
+                GridBuilder(4, 4, GameOptionsVariant.createClassic(DigitSetting.FIRST_DIGIT_ZERO))
+                    .addCageMultiply(0, GridCageType.ANGLE_RIGHT_TOP)
+                    .addCageSingle(2)
+                    .addCageMultiply(0, GridCageType.ANGLE_LEFT_BOTTOM)
+                    .addCageSingle(1)
+                    .addCageMultiply(0, GridCageType.L_HORIZONTAL_SHORT_RIGHT_BOTTOM)
+                    .addCageSubtract(2, GridCageType.DOUBLE_VERTICAL)
+                    .addCageSubtract(2, GridCageType.DOUBLE_HORIZONTAL)
+                    .createGrid()
+
+            grid.cells[0].possibles = setOf(1, 3)
+            grid.cells[1].userValue = 2
+            grid.cells[2].possibles = setOf(0, 3)
+            grid.cells[3].possibles = setOf(0, 1)
+            grid.cells[4].possibles = setOf(0, 2, 3)
+            grid.cells[5].possibles = setOf(0, 3)
+            grid.cells[6].userValue = 1
+            grid.cells[7].possibles = setOf(2, 3)
+            grid.cells[8].possibles = setOf(0, 1, 2, 3)
+            grid.cells[9].possibles = setOf(0, 1, 3)
+            grid.cells[10].possibles = setOf(0, 2, 3)
+            grid.cells[11].possibles = setOf(0, 1, 2, 3)
+            grid.cells[12].possibles = setOf(1, 2, 3)
+            grid.cells[13].possibles = setOf(0, 1, 3)
+            grid.cells[14].possibles = setOf(0, 2, 3)
+            grid.cells[15].possibles = setOf(0, 1, 2, 3)
+
+            println(grid)
+
+            val solver = NishioWithPairs()
+
+            solver.fillCellsWithNewCache(grid) shouldBe false
+        }
+
+        test("nishio at top right cell with value 4 leads to contradiction and gets value 1") {
+            val grid = createGridWithContradiction()
+
+            println(grid)
+
+            val solver = NishioWithPairs()
+
+            val solverResult = solver.fillCellsWithNewCacheReturningDetails(grid)
+            solverResult::class shouldBe HumanSolverStrategyResult.Success::class
+
+            grid.cells[2].userValue shouldBe 1
+
+            withClue("solver details") {
+                (solverResult as HumanSolverStrategyResult.Success).changedCells shouldContainExactlyInAnyOrder
+                    listOf(
+                        grid.cells[1],
+                        grid.cells[2],
+                        grid.cells[5],
+                    )
+            }
+        }
+
+        test("tryWithNishio finds contradiction") {
+            val grid = createGridWithContradiction()
+
+            println(grid)
+
+            val solver = NishioWithPairs()
+
+            solver.tryWithNishio(grid, grid.cells[2], 4)::class shouldBe NishioResult.Contradictions::class
+        }
+
+        test("fillCells fills all cells as the grid got solved") {
+            val grid = createGridToBeSolvedViaNishio()
+
+            println(grid)
+
+            val expectedChangedCells = grid.cells.filter { !it.isUserValueSet }
+
+            val solver = NishioWithPairs()
+
+            val solverResult = solver.fillCellsWithNewCacheReturningDetails(grid)
+            solverResult::class shouldBe HumanSolverStrategyResult.Success::class
+
+            grid.cells.all { it.isUserValueSet } shouldBe true
+
+            withClue("solver details") {
+                (solverResult as HumanSolverStrategyResult.Success).changedCells shouldContainExactlyInAnyOrder expectedChangedCells
+            }
+        }
+
+        test("tryWithNishio results nishio solved") {
+            val grid = createGridToBeSolvedViaNishio()
+
+            println(grid)
+
+            val solver = NishioWithPairs()
+
+            solver.tryWithNishio(grid, grid.cells[0], 1)::class shouldBe NishioResult.Solved::class
+        }
+    })
+
+private fun createGridWithContradiction(): Grid {
+    val grid =
+        GridBuilder(3, 4)
+            .addCageSubtract(2, GridCageType.DOUBLE_HORIZONTAL)
+            .addCageSubtract(3, GridCageType.DOUBLE_VERTICAL)
+            .addCageMultiply(36, GridCageType.TETRIS_HORIZONTAL_LEFT_TOP)
+            .addCageSubtract(1, GridCageType.DOUBLE_VERTICAL)
+            .addCageDivide(2, GridCageType.DOUBLE_HORIZONTAL)
+            .createGrid()
+
+    grid.cells[0].possibles = setOf(2, 3, 4)
+    grid.cells[1].possibles = setOf(1, 2, 4)
+    grid.cells[2].possibles = setOf(1, 4)
+    grid.cells[3].possibles = setOf(1, 2, 4)
+    grid.cells[4].userValue = 3
+    grid.cells[5].possibles = setOf(1, 4)
+    grid.cells[6].possibles = setOf(2, 4)
+    grid.cells[7].possibles = setOf(1, 2, 4)
+    grid.cells[8].userValue = 3
+    grid.cells[9].possibles = setOf(1, 3)
+    grid.cells[10].possibles = setOf(1, 4)
+    grid.cells[11].userValue = 2
+    return grid
+}
+
+private fun createGridToBeSolvedViaNishio(): Grid {
+    val grid =
+        GridBuilder(4, 3)
+            .addCageSubtract(1, GridCageType.DOUBLE_HORIZONTAL)
+            .addCageAdd(9, GridCageType.ANGLE_LEFT_BOTTOM)
+            .addCageSubtract(1, GridCageType.DOUBLE_VERTICAL)
+            .addCageMultiply(8, GridCageType.L_HORIZONTAL_SHORT_LEFT_TOP)
+            .addCageSingle(3)
+            .createGrid()
+
+    grid.cells[0].possibles = setOf(1, 2)
+    grid.cells[1].possibles = setOf(2, 3)
+    grid.cells[2].userValue = 4
+    grid.cells[3].possibles = setOf(1, 3)
+    grid.cells[4].possibles = setOf(2, 4)
+    grid.cells[5].userValue = 1
+    grid.cells[6].userValue = 3
+    grid.cells[7].possibles = setOf(2, 4)
+    grid.cells[8].userValue = 3
+    grid.cells[9].possibles = setOf(2, 4)
+    grid.cells[10].possibles = setOf(1, 2)
+    grid.cells[11].possibles = setOf(1, 2, 4)
+    return grid
+}
