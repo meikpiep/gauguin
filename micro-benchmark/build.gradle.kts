6023cb3873fc9b4cac7cdf338ab02e86bf731615
meikpiep <meik.public@piepmeyer.org>
2025-12-28 14:55:10 +0100

    fix usage of deprecated options since Kotlin 2.3.0

--- micro-benchmark/build.gradle.kts
+++ micro-benchmark/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        jvmToolchain(11)
     }
 }
 
