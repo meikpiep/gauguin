b55a94fbcca0d4897e5d6728a8f268acb6821f95
meikpiep <meik.public@piepmeyer.org>
2026-01-28 18:46:33 +0100

    enhance Human Solver coverage of NakedPair

--- gauguin-human-solver/src/test/kotlin/org/piepmeyer/gauguin/difficulty/human/strategy/NakedPairTest.kt
+++ gauguin-human-solver/src/test/kotlin/org/piepmeyer/gauguin/difficulty/human/strategy/NakedPairTest.kt
@@ -29,7 +29,6 @@ class NakedPairTest :
 
             println(grid)
 
-            // solver should find two possibles and delete one of them for each run
             solver.fillCells(grid, HumanSolverCacheImpl(grid)).madeChanges() shouldBe true
 
             println(grid)
@@ -40,5 +39,54 @@ class NakedPairTest :
                     grid.cells[3].possibles shouldContainExactly setOf(2, 4)
                 }
             }
+        }
+
+        test("1x4 grid rotated") {
+            val grid =
+                GridBuilder(1, 4)
+                    .addCageMultiply(3, GridCageType.DOUBLE_VERTICAL)
+                    .addCageSingle(2)
+                    .addCageSingle(4)
+                    .createGrid()
+
+            grid.cells[0].possibles = setOf(1, 3)
+            grid.cells[1].possibles = setOf(1, 3)
+            grid.cells[2].possibles = setOf(1, 2, 3, 4)
+            grid.cells[3].possibles = setOf(1, 2, 3, 4)
+
+            val solver = NakedPair()
+
+            println(grid)
+
+            solver.fillCells(grid, HumanSolverCacheImpl(grid)).madeChanges() shouldBe true
+
+            println(grid)
+
+            assertSoftly {
+                withClue("possibles should be deleted from other cells") {
+                    grid.cells[2].possibles shouldContainExactly setOf(2, 4)
+                    grid.cells[3].possibles shouldContainExactly setOf(2, 4)
+                }
+            }
+        }
+
+        test("1x4 grid rotated with pair but other cells already contain user value") {
+            val grid =
+                GridBuilder(1, 4)
+                    .addCageMultiply(3, GridCageType.DOUBLE_VERTICAL)
+                    .addCageSingle(2)
+                    .addCageSingle(4)
+                    .createGrid()
+
+            grid.cells[0].possibles = setOf(1, 3)
+            grid.cells[1].possibles = setOf(1, 3)
+            grid.cells[2].userValue = 2
+            grid.cells[3].userValue = 4
+
+            val solver = NakedPair()
+
+            println(grid)
+
+            solver.fillCells(grid, HumanSolverCacheImpl(grid)).madeChanges() shouldBe false
         }
     })
