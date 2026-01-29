8fa5447a093c929026f32539aba00a2fbed3a880
meikpiep <meik.public@piepmeyer.org>
2026-01-29 12:04:13 +0100

    extract GridPreviewCalculator

--- gauguin-core/src/main/kotlin/org/piepmeyer/gauguin/calculation/GridPreviewCalculationService.kt
+++ gauguin-core/src/main/kotlin/org/piepmeyer/gauguin/calculation/GridPreviewCalculationService.kt
@@ -4,18 +4,11 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.plus
-import kotlinx.coroutines.withTimeoutOrNull
-import org.piepmeyer.gauguin.creation.DifficultyAwareGridCreator
-import org.piepmeyer.gauguin.creation.GridCalculatorFactory
 import org.piepmeyer.gauguin.grid.Grid
-import org.piepmeyer.gauguin.options.DifficultySetting
 import org.piepmeyer.gauguin.options.GameVariant
-import org.piepmeyer.gauguin.options.SingleCageUsage
 import java.util.WeakHashMap
 
 private val logger = KotlinLogging.logger {}
@@ -25,7 +18,7 @@ class GridPreviewCalculationService(
 ) {
     private val grids: MutableMap<GameVariant, Grid> = WeakHashMap()
     private var listeners = mutableListOf<GridPreviewListener>()
-    private var lastGridCalculation: Deferred<Grid>? = null
+    private var previewCalculator: GridPreviewCalculator? = null
 
     fun getGrid(gameVariant: GameVariant): Grid? = grids[gameVariant]
 
@@ -38,57 +31,16 @@ class GridPreviewCalculationService(
         variant: GameVariant,
         scope: CoroutineScope,
     ) {
-        lastGridCalculation?.cancel()
-
-        var grid: Grid
-        var previewStillCalculating: Boolean
+        previewCalculator?.cancelCalculation()
 
         scope.launch(dispatcher) {
             with(this + CoroutineName("GridPreview-$variant")) {
-                logger.info { "Fetching real grid..." }
+                val calculator = GridPreviewCalculator(variant, listeners, this)
+                previewCalculator = calculator
 
-                val gridCalculation = async(CoroutineName("GridPreview-calculation-$variant")) { getOrCreateGrid(variant) }
-                lastGridCalculation = gridCalculation
-
-                val gridAfterShortTimeout = withTimeoutOrNull(250) { gridCalculation.await() }
-
-                if (gridAfterShortTimeout == null) {
-                    logger.info { "Generating pseudo grid..." }
-                    val variantWithoutDifficulty = pseudoGridPreviewVariant(variant)
-
-                    grid = DifficultyAwareGridCreator(variantWithoutDifficulty).createRandomizedGridWithCages()
-                    previewStillCalculating = true
-                    logger.info { "Finished generating pseudo grid." }
-                } else {
-                    logger.info { "Fetched real grid with short timeout." }
-                    grid = gridAfterShortTimeout
-                    previewStillCalculating = false
-                }
-
-                listeners.forEach { it.previewGridCreated(grid, previewStillCalculating) }
-
-                if (previewStillCalculating) {
-                    launch {
-                        val calculatedGrid = gridCalculation.await()
-                        listeners.forEach { it.previewGridCalculated(calculatedGrid) }
-                    }
-                }
+                calculator.calculateGrid(grids)
             }
         }
-    }
-
-    private suspend fun getOrCreateGrid(variant: GameVariant): Grid {
-        grids[variant]?.let {
-            logger.debug { "Returning already calculated grid." }
-            return it
-        }
-
-        logger.debug { "Calculating grid..." }
-        val grid = GridCalculatorFactory().createCalculator(variant).calculate()
-        grids[variant] = grid
-        logger.debug { "Grid calculated and stored." }
-
-        return grid
     }
 
     fun addListener(listener: GridPreviewListener) {
@@ -101,29 +53,12 @@ class GridPreviewCalculationService(
 
     fun clearGrids() {
         grids.clear()
-        lastGridCalculation = null
+
+        previewCalculator?.cancelCalculation()
+        previewCalculator = null
     }
 
     fun removeGrid(gridToRemove: Grid) {
         grids.remove(gridToRemove.variant)
-    }
-
-    companion object {
-        fun pseudoGridPreviewVariant(variant: GameVariant): GameVariant {
-            val variantWithoutDifficulty =
-                variant.copy(
-                    options =
-                        variant.options.copy(
-                            difficultiesSetting = DifficultySetting.all(),
-                            singleCageUsage =
-                                if (variant.options.singleCageUsage == SingleCageUsage.NO_SINGLE_CAGES) {
-                                    SingleCageUsage.DYNAMIC
-                                } else {
-                                    variant.options.singleCageUsage
-                                },
-                        ),
-                )
-            return variantWithoutDifficulty
-        }
     }
 }
