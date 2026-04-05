e2ceb9cf1fed6f92c757b8553dd4b823347533ab
meikpiep <meik.public@piepmeyer.org>
2026-04-05 21:01:27 +0200

    Upgrade Android Gradle Plugin from 9.0.1 to 9.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import java.net.URI
 
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:9.0.1")
+        classpath("com.android.tools.build:gradle:9.1.0")
     }
 }
 
