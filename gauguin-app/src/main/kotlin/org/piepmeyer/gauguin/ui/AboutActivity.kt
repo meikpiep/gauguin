9090d4d24e6cce8684ee7d57ae277290058edd5c
meikpiep <meik.public@piepmeyer.org>
2026-04-05 20:58:45 +0200

    wip

--- gauguin-app/src/main/kotlin/org/piepmeyer/gauguin/ui/AboutActivity.kt
+++ gauguin-app/src/main/kotlin/org/piepmeyer/gauguin/ui/AboutActivity.kt
@@ -8,12 +8,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 import org.koin.android.ext.android.inject
-import org.piepmeyer.gauguin.R
 import org.piepmeyer.gauguin.creation.GridBuilder
 import org.piepmeyer.gauguin.creation.cage.GridCageType
 import org.piepmeyer.gauguin.databinding.ActivityAboutBinding
 import org.piepmeyer.gauguin.game.Game
-import org.piepmeyer.gauguin.grid.GridCageAction
 
 class AboutActivity : AppCompatActivity() {
     private val activityUtils: ActivityUtils by inject()
@@ -55,33 +53,44 @@ class AboutActivity : AppCompatActivity() {
             clicksOnPicture++
 
             when (clicksOnPicture) {
-                2 -> Snackbar.make(binding.root, "There is no easter egg.", Snackbar.LENGTH_LONG).show()
-                3 -> Snackbar.make(binding.root, "There is really no easter egg.", Snackbar.LENGTH_LONG).show()
-                4 -> Snackbar.make(binding.root, "Only procceed if you don't mind loosing your current game.", Snackbar.LENGTH_LONG).show()
+                2 -> {
+                    Snackbar.make(binding.root, "There is no easter egg.", Snackbar.LENGTH_LONG).show()
+                }
+
+                3 -> {
+                    Snackbar.make(binding.root, "There is really no easter egg.", Snackbar.LENGTH_LONG).show()
+                }
+
+                4 -> {
+                    Snackbar.make(binding.root, "Only procceed if you don't mind loosing your current game.", Snackbar.LENGTH_LONG).show()
+                }
+
                 5 -> {
                     val grid =
                         GridBuilder(5, 5)
-                            .addCage(10, GridCageAction.ACTION_MULTIPLY, GridCageType.TRIPLE_HORIZONTAL, 2)
-                            .addCage(10, GridCageAction.ACTION_MULTIPLY, GridCageType.TRIPLE_VERTICAL, 6)
-                            .addCage(10, GridCageAction.ACTION_MULTIPLY, GridCageType.DOUBLE_HORIZONTAL, 2 + 20)
-                            .addSingleCage(1, 0)
-                            .addSingleCage(1, 1)
-                            .addSingleCage(1, 0 + 5)
-                            .addSingleCage(1, 2 + 5)
-                            .addSingleCage(1, 3 + 5)
-                            .addSingleCage(1, 4 + 5)
-                            .addSingleCage(1, 0 + 10)
-                            .addSingleCage(1, 2 + 10)
-                            .addSingleCage(1, 2 + 15)
-                            .addSingleCage(1, 3 + 15)
-                            .addCage(10, GridCageAction.ACTION_MULTIPLY, GridCageType.ANGLE_LEFT_BOTTOM, 3 + 10)
-                            .addSingleCage(1, 0 + 15)
-                            .addSingleCage(1, 0 + 20)
-                            .addSingleCage(1, 1 + 20)
-                            .addSingleCage(1, 4 + 20)
+                            .addCageSingle(1)
+                            .addCageSingle(1)
+                            .addCageMultiply(10, GridCageType.TRIPLE_HORIZONTAL)
+                            .addCageSingle(1)
+                            .addCageSingle(1)
+                            .addCageSingle(1)
+                            .addCageMultiply(10, GridCageType.TRIPLE_VERTICAL)
+                            .addCageSingle(1)
+                            .addCageSingle(1)
+                            .addCageSingle(1)
+                            .addCageSingle(1)
+                            .addCageSingle(1)
+                            .addCageMultiply(10, GridCageType.ANGLE_LEFT_BOTTOM)
+                            .addCageSingle(1)
+                            .addCageSingle(1)
+                            .addCageSingle(1)
+                            .addCageSingle(1)
+                            .addCageMultiply(10, GridCageType.DOUBLE_HORIZONTAL)
                             .createGrid()
 
-                    game.updateGrid(grid)
+                    grid.addPossiblesAtNewGame()
+
+                    game.useGrid(grid)
 
                     finishAfterTransition()
                 }
