ba525740a1628aefcbbbfd0cdd7afba39926a301
meikpiep <meik.public@piepmeyer.org>
2026-06-04 18:01:09 +0200

    refactoring: inner class refactoring of DigitSetting

--- gauguin-core/src/main/kotlin/org/piepmeyer/gauguin/options/DigitSetting.kt
+++ gauguin-core/src/main/kotlin/org/piepmeyer/gauguin/options/DigitSetting.kt
@@ -2,16 +2,11 @@ package org.piepmeyer.gauguin.options
 
 import org.piepmeyer.gauguin.grid.GridSize
 
-private fun allNumbersBetween(
-    lowNumber: Int,
-    highNumber: Int,
-): Set<Int> = (lowNumber..highNumber).toSet()
-
 enum class DigitSetting(
     val numbers: Set<Int>,
 ) {
-    FIRST_DIGIT_ONE(allNumbersBetween(1, 12)),
-    FIRST_DIGIT_ZERO(allNumbersBetween(0, 11)),
+    FIRST_DIGIT_ONE((1..12).toSet()),
+    FIRST_DIGIT_ZERO((0..11).toSet()),
     PRIME_NUMBERS(setOf(1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31)),
     FIBONACCI_SEQUENCE(
         setOf(
@@ -33,9 +28,9 @@ enum class DigitSetting(
         setOf(1, 2, 3, 4, 5, 7, 9, 12, 16, 21, 28, 37),
     ),
     FIRST_DIGIT_MINUS_TWO(
-        allNumbersBetween(-2, 9),
+        (-2..9).toSet(),
     ),
-    FIRST_DIGIT_MINUS_FIVE(allNumbersBetween(-5, 6)),
+    FIRST_DIGIT_MINUS_FIVE((-5..6).toSet()),
     ;
 
     fun getPossibleDigits(gridSize: GridSize): Set<Int> = numbers.take(gridSize.amountOfNumbers).toSet()
