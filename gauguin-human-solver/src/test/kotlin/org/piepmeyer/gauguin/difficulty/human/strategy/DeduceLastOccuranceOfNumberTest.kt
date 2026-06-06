239b31c11bf3dada7020ad01e60862142fa98235
meikpiep <meik.public@piepmeyer.org>
2026-06-06 12:04:05 +0200

    human solver: add integration test to ensure an example 7x7 grid gets solved regarding all occurrences of possible number 7

---
+++ gauguin-human-solver/src/test/kotlin/org/piepmeyer/gauguin/difficulty/human/strategy/DeduceLastOccuranceOfNumberTest.kt
@@ -0,0 +1,77 @@
+package org.piepmeyer.gauguin.difficulty.human.strategy
+
+import io.kotest.assertions.withClue
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import org.piepmeyer.gauguin.creation.GridBuilder
+import org.piepmeyer.gauguin.creation.cage.GridCageType
+import org.piepmeyer.gauguin.difficulty.human.HumanSolver
+
+class DeduceLastOccuranceOfNumberTest :
+    FunSpec({
+
+        test("7x7 grid, last 7 can be set in cell on right column") {
+            val grid =
+                GridBuilder(7, 7)
+                    .addCageMultiply(
+                        84,
+                        GridCageType.TRIPLE_HORIZONTAL,
+                    ).addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageMultiply(28, GridCageType.DOUBLE_VERTICAL)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingle(7)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageMultiply(
+                        392,
+                        GridCageType.SQUARE,
+                    ).addCageSingleAllPossibles(1)
+                    .addCageMultiply(
+                        84,
+                        GridCageType.L_HORIZONTAL_SHORT_LEFT_TOP,
+                    ).addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .addCageSingleAllPossibles(1)
+                    .createGrid()
+
+            val solver = HumanSolver(grid, false, true)
+            solver.prepareGrid()
+
+            println(grid)
+
+            solver.solveAndCalculateDifficulty(avoidReveal = true)
+
+            println(grid)
+
+            withClue("cell 27 should got the use value 7 as this is the only cell left to hold a seven regarindg the last column") {
+                grid.cells[27].userValue shouldBe 7
+            }
+        }
+    })
